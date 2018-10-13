70fa44426839ca4984634fa9605dc46a43f2664d
jeffbroadhurst18 <jeffbroadhurst18@outlook.com>
2018-10-13 16:36:17 +0100

    Further work on data access

--- ContosoUniversity/Pages/Instructors/Index.cshtml.cs
+++ ContosoUniversity/Pages/Instructors/Index.cshtml.cs
@@ -30,26 +30,39 @@ namespace ContosoUniversity.Pages.Instructors
 									.Include(i => i.CourseAssignments)
 									.ThenInclude(i => i.Course)
 									.ThenInclude(i => i.Department)
-									.Include(i => i.CourseAssignments)
-									.ThenInclude(i => i.Course)
-									.ThenInclude(i => i.Enrollments)
-									.ThenInclude(i => i.Student)
-									.AsNoTracking().OrderBy(i => i.LastName)
+									//.Include(i => i.CourseAssignments)
+									//.ThenInclude(i => i.Course)
+									//.ThenInclude(i => i.Enrollments)
+									//.ThenInclude(i => i.Student)
+									//.AsNoTracking()
+									.OrderBy(i => i.LastName)
 									.ToListAsync();
 
 			if (id != null)
 			{
 				InstructorID = id.Value;
-				Instructor instructor = Instructor.Instructors.Where(
-					i => i.ID == id.Value).Single(); //get single value of selected instructor
+				//Instructor instructor = Instructor.Instructors.Where(
+				//	i => i.ID == id.Value).Single(); //get single value of selected instructor
+				Instructor instructor = Instructor.Instructors.Single(i => i.ID == id.Value);
 				Instructor.Courses = instructor.CourseAssignments.Select(c => c.Course); //Get a list of courses
 			}
 
-			if (courseID != null)
+			/*if (courseID != null)
 			{
 				CourseID = courseID.Value;
-				Instructor.Enrollments = Instructor.Courses.Where(x => x.CourseID == courseID).Single().Enrollments;
+				//Instructor.Enrollments = Instructor.Courses.Where(x => x.CourseID == courseID).Single().Enrollments;
+				Instructor.Enrollments = Instructor.Courses.Single(x => x.CourseID == courseID).Enrollments;
 				//Get enrollments for the seelected course
+			}*/
+			if (courseID != null)
+			{
+				var selectedCourse = Instructor.Courses.Where(x => x.CourseID == courseID).Single(); //Single converts from being a list to a single object
+				await _context.Entry(selectedCourse).Collection(x => x.Enrollments).LoadAsync(); //Loads entities linked to main entity
+				foreach (Enrollment enrollment in selectedCourse.Enrollments)
+				{
+					await _context.Entry(enrollment).Reference(x => x.Student).LoadAsync(); //Loads a single field
+				}
+				Instructor.Enrollments = selectedCourse.Enrollments;
 			}
         }
     }
